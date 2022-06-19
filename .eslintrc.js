8accfdf45b06b68a7f042f01b4070f8c5f323232
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-06-19 15:48:52 -0500

    add eslint, prettier and husky config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+const RULES = {
+  OFF: 'off',
+  ERROR: 'error',
+  WARN: 'warn',
+};
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': RULES.ERROR,
+  },
+};
