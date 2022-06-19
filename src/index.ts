d5fabadd229930c77c6c2757b66ee871a03a8eae
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-06-19 15:08:51 -0500

    add server config

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+// Initial config
+dotenv.config();
+const PORT: string | undefined = process.env.APP_PORT;
+const HOST: string | undefined = process.env.APP_HOST;
+console.log(process.env.APP_PORT);
+
+// add express instance
+const app = express();
+
+// add midlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// start server
+app.listen(PORT, (): void => {
+  const url = `${HOST}:${PORT}`;
+  console.log(`Server is running on: ${url}`);
+});
